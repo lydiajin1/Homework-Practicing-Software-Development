05b9e3bf42178459fea1b7d8b876b6d9f4b075fd
Lydia Jin (RIT Student) <lj2733@rit.edu>
2024-11-09 15:40:59 -0500

    added the general outline of what a phone book consists of

---
+++ practicing_software_development/practicing_software_development/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace practicing_software_development
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
